ebd69c045e0111a309d80fc3add758490bcb4c66
Manojkumar144 <manojsantha1442000@gmail.com>
2023-10-17 20:34:10 +0530

    created a fun application

---
+++ script.js
@@ -0,0 +1,33 @@
+const moveButton = document.getElementById('moveButton');
+
+function moveButtonRandomly() {
+    const maxWidth = window.innerWidth - moveButton.clientWidth;
+    const maxHeight = window.innerHeight - moveButton.clientHeight;
+
+    const randomX = Math.floor(Math.random() * maxWidth);
+    const randomY = Math.floor(Math.random() * maxHeight);
+
+    moveButton.style.left = `${randomX}px`;
+    moveButton.style.top = `${randomY}px`;
+}
+
+moveButton.addEventListener('click', moveButtonRandomly);
+
+
+
+const showButton = document.getElementById('yesButton');
+const gifImage = document.getElementById('gifImage');
+
+showButton.addEventListener('click', function () {
+    var para = document.createElement('p');
+    para.innerText='haha! I knew it. Address: dubai, Dubai kuruku theru...varum pothu kasu eduthutu vanthurunga..:))';
+
+    var container = document.getElementById('dog'); 
+
+container.appendChild(para);
+    if (gifImage.classList.contains('hidden')) {
+        gifImage.classList.remove('hidden');
+    } else {
+        gifImage.classList.add('hidden');
+    }
+});
